89f8f59d9b2b3647cc90c3cf551f85846ef79252
Jonathan Damasceno <contato.jonathandamasceno@gmail.com>
2025-11-16 17:32:06 -0300

    vertical q7

--- JonathanDamasceno-20251160031-T1/trabalho1.c
+++ JonathanDamasceno-20251160031-T1/trabalho1.c
@@ -370,6 +370,8 @@ int q6(int numerobase, int numerobusca)
          palavraInvertida[aux] = palavra[c];
       }
 
+
+      // horizontal
       // esquerda -> direita || direita -> esquerda (pode estar invertida)
       achou = 0;
       for(iCont=0; iCont < 8; iCont++){
@@ -387,6 +389,25 @@ int q6(int numerobase, int numerobusca)
             }
          }
       }
+
+      // vertical 
+      // cima -> baixo, baixo -> cima
+      for(iCont=0; iCont < 8; iCont++){ // 0
+         for(jCont=0; jCont<10; jCont++){ // 00 01
+            if(matriz[iCont][jCont] == palavra[0]){
+               for(kCont=0; kCont < lenPalavra; kCont++){
+                  if(matriz[iCont + jCont][jCont] != palavra[kCont] && matriz[iCont + jCont][jCont] != palavraInvertida[kCont]){
+                     break;
+                  }
+               }
+            }  
+            if(kCont == lenPalavra){
+               achou = 1;
+               break;
+            }
+         }
+      }
+      // caso passe
       return achou;
  }
 
